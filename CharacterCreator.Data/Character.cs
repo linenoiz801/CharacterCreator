ef890175580415a06b825df6ce75c4f02883c201
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-08 18:45:05 -0400

    added data annotations to properties

--- CharacterCreator.Data/Character.cs
+++ CharacterCreator.Data/Character.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CharacterCreator.Data
 {
-    class Character
+    public class Character
     {
+        [Key]
+        public int CharacterId { get; set; }
+        // [ForeignKey(nameof(PhysicalAttributes))]
+        //public int PhysicalAttributesId {get; set;}
+        //public virtual PhysicalAttribute PhysicalAttributes {get; set;}
+
+        //[ForeignKey(nameof(Skills))]
+        //public int SkillsId {get; set}
+        //public virtual Skill Skills {get; set;}
+
+        //public enum Race TypeOfRace {get; set}
+        //public enum Skill TypeOfSkill {get;set;}
+        
+        [Required]
+        public string History { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
     }
 }
