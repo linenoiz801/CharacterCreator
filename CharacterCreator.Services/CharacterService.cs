e74be1bea773d37acff3a418ba126dfcc9d6ea9a
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-08 19:18:59 -0400

    started working on CreateCharacter method in CharacterService

--- CharacterCreator.Services/CharacterService.cs
+++ CharacterCreator.Services/CharacterService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CharacterCreator.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace CharacterCreator.Services
 {
-    class CharacterService
+    public class CharacterService
     {
+        public bool CreateCharacter(CharacterCreate model)
+        {
+
+        }
     }
 }
