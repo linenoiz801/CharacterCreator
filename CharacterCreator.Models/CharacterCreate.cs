e7536d4b06fe61a52abd709f97a5db570559f526
Adrianna Albertson <AnnaAlbertson7@gmail.com>
2021-05-08 19:03:28 -0400

    created properties for CharacterCreate in .Models

--- CharacterCreator.Models/CharacterCreate.cs
+++ CharacterCreator.Models/CharacterCreate.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace CharacterCreator.Models
 {
-    class CharacterCreate
+    public class CharacterCreate
     {
+        public int PhysicalAttributesId {get; set;}
+        public int SkillsId { get; set; }
+        public string History { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        //public List<string> Race {get; set}
+        //public List<string> Skill {get;set;}
     }
 }
